52538a9ca3c44fc02660aa83a036f8c5d091984b
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-02-02 13:33:06 +0100

    template form

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  credentials = {
+    email: '',
+    password: ''
+  }
   constructor() { }
 
   ngOnInit(): void {
