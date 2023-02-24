382475c58fcf42b86301ba509d8db2ea3f999e23
Kehinde-Agboola <kehindeagboola52@gmail.com>
2023-02-24 14:50:58 +0100

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,10 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+  apiKey: "AIzaSyB3DrxVWbZrEg7RvOftgFgVNba9FOzpXQ8",
+  authDomain: "clips-4c90a.firebaseapp.com",
+  projectId: "clips-4c90a",
+  storageBucket: "clips-4c90a.appspot.com",
+  appId: "1:389887726220:web:3091d4efdf88518cc621a9"
+};
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+  apiKey: "AIzaSyB3DrxVWbZrEg7RvOftgFgVNba9FOzpXQ8",
+  authDomain: "clips-4c90a.firebaseapp.com",
+  projectId: "clips-4c90a",
+  storageBucket: "clips-4c90a.appspot.com",
+  appId: "1:389887726220:web:3091d4efdf88518cc621a9"
+};
 };
 
 /*
